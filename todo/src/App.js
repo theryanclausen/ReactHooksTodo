9ade151545e5fa77fd6786bacbd25b90d7e5caf7
theryanclausen <theryanclausen@gmail.com>
2018-11-21 22:19:55 -0700

    basic hooks implimentation. More dependencies

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,14 +1,27 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [data, setData] = useState({ todos: [] });
+  const fetch = async () => {
+    const todos = await axios("http://localhost:3334/api/todos");
+    if(todos.data !== data.todos){
+     setData({ todos: todos.data }); 
+    }
+  };
+  useEffect(() => {
+    fetch() ;
+  });
+
+  return (
+    <div>
+      {data.todos.map(todo => (
+        <div key={todo.id}>
+          <h1>{todo.name}</h1>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default App;
